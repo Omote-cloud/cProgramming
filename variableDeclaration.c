2527535021d721ffb78cd7e05f7a59d7affd48ca
Omote <immanuelomote@gmail.com>
2022-08-25 11:20:29 +0000

    Variable Declaration using typedef

--- variableDeclaration.c
+++ variableDeclaration.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-struct Point
+typedef struct Point
 {
     int x;
     int y;
-};
+}Point;
 
 int main()
 {
-    struct Point p1;
+    Point p1;
     p1.x = 1;
     p1.y = 3;
-    
+
     printf("%d \n", p1.x);
     printf("%d \n", p1.y);
     return 0;
